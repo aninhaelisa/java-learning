5a58c69ed4ec31861485f35cea8bffdc7fd25c0a
aninhaelisa <elisaana.2019.2006@gmail.com>
2026-05-22 16:00:23 -0300

    Herança

---
+++ projetosBasicos/heranca/App.java
@@ -0,0 +1,13 @@
+package projetosBasicos.heranca;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+}
