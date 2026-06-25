7d9b46e0edc053e0f0e6da5db602d46a02f83122
aninhaelisa <elisaana.2019.2006@gmail.com>
2026-06-25 18:01:41 -0300

    inicio sobre interfaces - básico

---
+++ projetosBasicos/interfaceBasico/entities/model/CarRental.java
@@ -0,0 +1,52 @@
+package projetosBasicos.interfaceBasico.entities.model;
+
+import java.time.LocalDateTime;
+
+public class CarRental{
+    private LocalDateTime start;
+    private LocalDateTime finish;
+
+    private Veiculo veiculo;
+    private Fatura fatura;
+
+    public CarRental() {
+    }
+
+    public CarRental(LocalDateTime start, LocalDateTime finish, Veiculo veiculo) {
+        this.start = start;
+        this.finish = finish;
+        this.veiculo = veiculo;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public LocalDateTime getFinish() {
+        return finish;
+    } 
+
+    public void setFinish(LocalDateTime finish) {
+        this.finish = finish;
+    }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public Fatura getFatura() {
+        return fatura;
+    }
+
+    public void setFatura(Fatura fatura) {
+        this.fatura = fatura;
+    }
+}
---
+++ projetosBasicos/interfaceBasico/entities/model/Veiculo.java
@@ -0,0 +1,20 @@
+package projetosBasicos.interfaceBasico.entities.model;
+
+public class Veiculo {
+    private String model;
+
+    public Veiculo() {
+    }
+
+    public Veiculo(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
