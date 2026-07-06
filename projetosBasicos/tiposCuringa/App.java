8a3f4bd1f1faae842779e69f9564d84e4f90b914
aninhaelisa <elisaana.2019.2006@gmail.com>
2026-07-06 13:37:56 -0300

    tipos curingas delimitados

--- projetosBasicos/tiposCuringa/App.java
+++ projetosBasicos/tiposCuringa/App.java
@@ -7,11 +7,15 @@ public class App {
     public static void main(String[] args) {
         List<?> lista = new ArrayList<>(); // ? é um tipo curinga, que pode ser qualquer tipo de objeto
         List<Integer> myNumbers = new ArrayList<>();
-        lista = myNumbers; 
+        lista = myNumbers;
 
-       // lista.add(3); // isso gera erro pois a lista é do tipo curinga, e não sabemos qual é o tipo real da lista. Portanto, não podemos adicionar elementos a ela.
+        // lista.add(3); // isso gera erro pois a lista é do tipo curinga, e não sabemos
+        // qual é o tipo real da lista. Portanto, não podemos adicionar elementos a ela.
 
+        // Tipos curingas delimitados
+        List<? extends Number> lista2 = new ArrayList<>();
+        List<Integer> myNumbers2 = new ArrayList<>();
+        lista2 = myNumbers2; // isso é permitido, pois Integer é um subtipo de Number
 
-       List<Integer> myInts = new ArrayList<>(5, 6, 7);
     }
 }
