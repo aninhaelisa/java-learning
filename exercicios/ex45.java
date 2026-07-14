ff5f731c5998c19934cf02b7c24e82fed0934e0d
aninhaelisa <elisaana.2019.2006@gmail.com>
2026-07-14 16:55:48 -0300

    EXERCICIO 45 - CADASTRO ALUNO (ARQUIVO .CSV

--- exercicios/ex45.java
+++ exercicios/ex45.java
@@ -6,6 +6,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
@@ -37,12 +38,12 @@ public class ex45 {
                     System.out.print("NOME ALUNO: ");
                     String nomeAluno = sc.nextLine();
 
-                    Object[] line1 = { nomeAluno};
-
                     try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
-                        for (Object l : line1) {
-                            bw.write(String.valueOf(l));
+                        if (new File(path).length() > 0) {
+                            bw.newLine();
                         }
+
+                        bw.write(nomeAluno);
 
                     } catch (Exception e) {
                         System.out.println();
@@ -71,7 +72,11 @@ public class ex45 {
                     System.out.print("NOTA DE ESPORTES: ");
                     double notaEsport = sc.nextDouble();
 
+                    double media = (notaMat + notaPort + notaCien + notaArt + notaEsport) / 5;
+
                     List<String> line2 = Files.readAllLines(Paths.get(path));
+
+                    boolean alunoEncontrado = false;
 
                     for (int i = 0; i < line2.size(); i++) {
 
@@ -79,16 +84,134 @@ public class ex45 {
 
                         if (linha.startsWith(nomeAdicionar + ",") || linha.equals(nomeAdicionar)) {
 
-                            linha += slip + notaMat + slip + notaPort + slip + notaCien + slip + notaArt + slip + notaEsport;
+                            linha += slip + notaMat + slip + notaPort + slip + notaCien + slip + notaArt + slip
+                                    + notaEsport + slip + media;
                             line2.set(i, linha);
+
+                            alunoEncontrado = true;
                             break;
                         }
                     }
 
-                    Files.write(Paths.get(path), line2);
-                    System.out.println("NOTAS ADICIONADAS COM SUCESSO!");
+                    if (alunoEncontrado) {
+                        Files.write(Paths.get(path), line2);
+                        System.out.println("NOTAS ADICIONADAS COM SUCESSO!");
+                    } else {
+                        System.out.println("ALUNO NAO ENCONTRADO!");
+                    }
 
                     System.out.println();
+                    break;
+
+                case 3:
+                    sc.nextLine();
+                    System.out.print("Nome do aluno que deseja alterar a nota: ");
+                    String nomeAlterar = sc.nextLine();
+
+                    List<String> notas = null;
+                    int linhaIndex = -1;
+                    List<String> line3 = Files.readAllLines(Paths.get(path));
+                    alunoEncontrado = false;
+
+                    for (int i = 0; i < line3.size(); i++) {
+                        String linha = line3.get(i);
+                        if (linha.startsWith(nomeAlterar + ",") || linha.equals(nomeAlterar)) {
+                            alunoEncontrado = true;
+                            notas = Arrays.asList(linha.split(","));
+                            linhaIndex = i;
+                            break;
+                        }
+                    }
+
+                    if (alunoEncontrado) {
+                        System.out.println("Aluno encontrado! Qual nota deseja alterar?");
+                        System.out.println("1 - Matematica | 2 - Portugues | 3 - Ciencias | 4 - Artes | 5 - Esportes");
+                        System.out.print(nomeUser + "> ");
+                        int notaAlterar = sc.nextInt();
+                        System.out.println("Você selecionou a nota: " + notaAlterar + " do aluno " + nomeAlterar);
+                        System.out.print("Digite a nova nota: ");
+                        double novaNota = sc.nextDouble();
+                        notas.set(notaAlterar, String.valueOf(novaNota));
+                        String novaLinha = String.join(",", notas);
+                        line3.set(linhaIndex, novaLinha);
+                        Files.write(Paths.get(path), line3);
+
+                    } else {
+                        System.out.println("Aluno não encontrado!");
+                    }
+                    break;
+
+                case 4:
+                    sc.nextLine();
+                    System.out.print("Nome do aluno que deseja remover: ");
+                    String nomeRemover = sc.nextLine();
+
+                    List<String> line4 = Files.readAllLines(Paths.get(path));
+                    alunoEncontrado = false;
+
+                    for (int i = 0; i < line4.size(); i++) {
+                        String linha = line4.get(i);
+                        if (linha.startsWith(nomeRemover + ",") || linha.equals(nomeRemover)) {
+                            line4.remove(i);
+                            alunoEncontrado = true;
+                            break;
+                        }
+                    }
+
+                    if (alunoEncontrado) {
+                        Files.write(Paths.get(path), line4);
+                        System.out.println("Aluno removido com sucesso!");
+                    } else {
+                        System.out.println("Aluno não encontrado!");
+                    }
+
+                    System.out.println();
+                    break;
+
+                case 5:
+                    List<String> line5 = Files.readAllLines(Paths.get(path));
+                    System.out.println("LISTA DE ALUNOS E NOTAS:");
+                    System.out.println("NOME | MATEMATICA | PORTUGUES | CIENCIAS | ARTES | ESPORTES | MEDIA");
+                    for (String linha : line5) {
+                        String[] dados = linha.split(",");
+                        System.out.print("Aluno: " + dados[0]);
+                        if (dados.length > 1) {
+                            System.out.print(" | Notas: ");
+                            for (int i = 1; i < dados.length; i++) {
+                                System.out.print(dados[i] + " | ");
+                            }
+                        }
+                        System.out.println();
+                    }
+                    System.out.println();
+                    break;
+
+                case 6:
+                    sc.nextLine();
+                    System.out.print("Nome do aluno que deseja buscar: ");
+                    String nomeBuscar = sc.nextLine();
+
+                    List<String> line6 = Files.readAllLines(Paths.get(path));
+                    alunoEncontrado = false;
+
+                    for (String linha : line6) {
+                        if (linha.startsWith(nomeBuscar + ",") || linha.equals(nomeBuscar)) {
+                            System.out.println("NOME | MATEMATICA | PORTUGUES | CIENCIAS | ARTES | ESPORTES | MEDIA");
+                            System.out.println("Aluno encontrado: " + linha);
+                            alunoEncontrado = true;
+                            break;
+                        }
+                    }
+
+                    if (!alunoEncontrado) {
+                        System.out.println("Aluno não encontrado!");
+                    }
+
+                    System.out.println();
+                    break;
+                case 0:
+                    System.out.println(":)     | PROGRAMA ENCERRADO |     :)");
+                    break;
                 default:
                     break;
             }
