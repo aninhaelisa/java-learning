6c76ada83043e008bb80f07418ac9c18a6af0340
aninhaelisa <elisaana.2019.2006@gmail.com>
2026-07-23 18:14:21 -0300

    Inicio exemplo predicate

---
+++ projetosBasicos/predicateExemplo/App.java
@@ -0,0 +1,11 @@
+package projetosBasicos.predicateExemplo;
+
+import projetosBasicos.predicateExemplo.entities.Produto;
+
+public class App {
+
+    public static void main(String[] args) {
+        Produto p = new Produto("Cama", 2222.00);
+        System.out.println(p);
+    }
+}
