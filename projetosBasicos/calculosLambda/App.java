a4a525371a0f8b83273e130b365d824d3a9661b8
aninhaelisa <elisaana.2019.2006@gmail.com>
2026-07-22 22:58:52 -0300

    Transparencia referencial

---
+++ projetosBasicos/calculosLambda/App.java
@@ -0,0 +1,26 @@
+package projetosBasicos.calculosLambda;
+
+import java.util.Arrays;
+
+public class App {
+
+    public static int globalValue = 3;
+
+    public static void main(String[] args) {
+        int[] vect = new int[] { 3, 4, 5 };
+        changeOddValues(vect);
+        System.out.println(Arrays.toString(vect));
+
+    }
+
+    public static void changeOddValues(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 != 0) {
+                numbers[i] += globalValue;
+            }
+        }
+    }
+}
+
+// Uma função é transparente se, para os mesmos argumentos, ela sempre produz o
+// mesmo resultado e não depende de estado externo.
