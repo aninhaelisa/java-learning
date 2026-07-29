620fe9ee9d16b3487fa37e0d158961a42f5bf4e2
aninhaelisa <elisaana.2019.2006@gmail.com>
2026-07-29 15:55:54 -0300

    Implementando lambda básico

--- projetosBasicos/expressoesLambdaAula/App.java
+++ projetosBasicos/expressoesLambdaAula/App.java
@@ -3,10 +3,12 @@ package projetosBasicos.expressoesLambdaAula;
 public class App {
     public static void main(String[] args) {
         Cat myCat = new Cat();
-        printThing(myCat);
+
+        Printable printableLambda = () -> System.out.println("MEOW");
+        printThing(printableLambda);
     }
 
-    static void printThing(Printable thing){
+    static void printThing(Printable thing) {
         thing.print();
     }
 }
