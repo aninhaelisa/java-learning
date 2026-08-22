9c357b92d3b345c78fe7e5719a4edd56d56ee0ee
aninhaelisa <elisaana.2019.2006@gmail.com>
2026-08-21 22:17:56 -0300

    Recuperar Dados

---
+++ projetosBasicos/recuperarDadosJDBC/App.java
@@ -0,0 +1,35 @@
+package projetosBasicos.recuperarDadosJDBC;
+
+import java.sql.Statement;
+
+import projetosBasicos.recuperarDadosJDBC.db.DB;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class App {
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try{
+            conn = DB.getConnection();
+            st = conn.createStatement();
+
+            rs = st.executeQuery("select * from department");
+
+            while(rs.next()){
+                System.out.println(rs.getInt("Id") + "," + rs.getString(("Name")));
+            }
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }finally{
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+            DB.closeConnection();
+        }
+    }
+}
