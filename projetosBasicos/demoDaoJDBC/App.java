977d4925a07cc7eb9f4bdb1bddca0e95b3b40726
aninhaelisa <elisaana.2019.2006@gmail.com>
2026-08-24 23:03:18 -0300

    Implementação da classe Department

--- projetosBasicos/demoDaoJDBC/App.java
+++ projetosBasicos/demoDaoJDBC/App.java
@@ -1,7 +1,10 @@
 package projetosBasicos.demoDaoJDBC;
 
+import projetosBasicos.demoDaoJDBC.model.entities.Department;
+
 public class App {
     public static void main(String[] args) {
-        
+        Department departmentOBJ = new Department(1, "Books");
+        System.out.println(departmentOBJ);
     }
 }
