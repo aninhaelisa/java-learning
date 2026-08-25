dbd69c3e8b8ea9f2e15c932d1d9472deea9ce661
aninhaelisa <elisaana.2019.2006@gmail.com>
2026-08-25 15:34:45 -0300

    Implementação findById()

--- projetosBasicos/demoDaoJDBC/App.java
+++ projetosBasicos/demoDaoJDBC/App.java
@@ -9,10 +9,10 @@ import projetosBasicos.demoDaoJDBC.model.entities.Seller;
 
 public class App {
     public static void main(String[] args) {
-        Department departmentOBJ = new Department(1, "Books");
-        Seller sellerOBJ = new Seller(21, "Ana", "ana2026@email.com", new Date(), 3000.55, departmentOBJ );
+    
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
         
-        System.out.println(sellerOBJ);
+        System.out.println(seller);
     }
 }
--- projetosBasicos/demoDaoJDBC/model/dao/DaoFactory.java
+++ projetosBasicos/demoDaoJDBC/model/dao/DaoFactory.java
@@ -1,9 +1,10 @@
 package projetosBasicos.demoDaoJDBC.model.dao;
 
 import projetosBasicos.demoDaoJDBC.model.dao.impl.SellerDaoJDBC;
+import projetosBasicos.demoDaoJDBC.db.DB;
 
 public class DaoFactory {
     public static SellerDao createSellerDao(){
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
