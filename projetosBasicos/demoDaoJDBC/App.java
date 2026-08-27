fdb025c7b913874d54f0b819d0ff69a6595c9388
aninhaelisa <elisaana.2019.2006@gmail.com>
2026-08-27 15:53:31 -0300

    Implementação do Update

--- projetosBasicos/demoDaoJDBC/App.java
+++ projetosBasicos/demoDaoJDBC/App.java
@@ -10,7 +10,7 @@ import projetosBasicos.demoDaoJDBC.model.entities.Seller;
 
 public class App {
     public static void main(String[] args) {
-    
+
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("--".repeat(10) + "TESTE 1 SELLER FINDBYID: " + "--".repeat(10));
@@ -21,14 +21,14 @@ public class App {
         System.out.println("--".repeat(10) + "TESTE 2 SELLER FINDBYDEPARTMENT: " + "--".repeat(10));
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
-        for(Seller obj: list){
+        for (Seller obj : list) {
             System.out.println(obj);
         }
 
         System.out.println();
         System.out.println("--".repeat(10) + "TESTE 3 SELLER FINDBYALL: " + "--".repeat(10));
         list = sellerDao.findAll();
-        for(Seller obj: list){
+        for (Seller obj : list) {
             System.out.println(obj);
         }
 
@@ -37,5 +37,12 @@ public class App {
         Seller newSeller = new Seller(null, "Grag", "grag@email.com", new Date(), 4000.00, department);
         sellerDao.insert(newSeller);
         System.out.println("INSERIDO! New Id: " + newSeller.getId());
+
+        System.out.println();
+        System.out.println("--".repeat(10) + "TESTE 5 SELLER upadate: " + "--".repeat(10));
+        seller = sellerDao.findById(1);
+        seller.setName("Marta Waine");
+        sellerDao.update(seller);
+        System.out.println("Update Completo! ");
     }
 }
