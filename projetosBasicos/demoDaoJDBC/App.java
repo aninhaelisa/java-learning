f9f17e521f2fd6a6b8237cf4674bdbf878b7bc03
aninhaelisa <elisaana.2019.2006@gmail.com>
2026-08-26 21:07:45 -0300

    Reutilizando instanciação

--- projetosBasicos/demoDaoJDBC/App.java
+++ projetosBasicos/demoDaoJDBC/App.java
@@ -11,8 +11,9 @@ public class App {
     public static void main(String[] args) {
     
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println("--".repeat(10) + "TESTE 1 SELLER FINDBYID: " + "--".repeat(10));
         Seller seller = sellerDao.findById(3);
-        
         System.out.println(seller);
     }
 }
