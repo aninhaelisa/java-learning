c5e094fdeb44b773e6450535617d40ec862abc67
aninhaelisa <elisaana.2019.2006@gmail.com>
2026-09-09 18:28:18 -0300

    Primeira aplicação springboot basica

---
+++ springboot/src/main/java/exemplo/controller/TesteController.java
@@ -0,0 +1,13 @@
+package exemplo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteController {
+
+    @GetMapping("/teste")
+    public String teste() {
+        return "Spring Boot funcionando!";
+    }
+}
