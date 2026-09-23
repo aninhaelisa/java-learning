c851fe5d7fd7b3df5a9dd8ffe6423d3f770f2d4a
aninhaelisa <elisaana.2019.2006@gmail.com>
2026-09-23 17:30:31 -0300

    Entidade Pedido. Datas com Instant e padrão ISO 8601 - PARTE 1

---
+++ projetos-Spring/curso/src/main/java/com/educandoweb/curso/entities/Order.java
@@ -0,0 +1,80 @@
+package com.educandoweb.curso.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity 
+@Table (name = "tb_order")
+public class Order implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id 
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @ManyToOne
+    @JoinColumn (name = "client_id")
+    private User client;
+
+    public Order() {
+    }
+
+    public Order(Long id, Instant moment, User client) {
+        this.id = id;
+        this.moment = moment;
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Order other = (Order) obj;
+        return id != null && id.equals(other.id);
+    }
+
+    public String toString() {
+        return "Order [id=" + id + ", moment=" + moment + ", client=" + client + "]";
+    }
+}
